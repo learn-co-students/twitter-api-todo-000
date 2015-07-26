089ec438476c1d3e39846bf2e994c1188f605601
Artem Eremin <artross.bodhi@gmail.com>
2015-07-26 11:22:21 +0300

    done

--- twitter.rb
+++ twitter.rb
@@ -13,17 +13,22 @@ class TwitterApi
       config.access_token        = keys['ACCESS_TOKEN']
       config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
     end
+
+    @username = "artross_test"
   end
 
   def most_recent_follower
     # find the Twitter gem method that accomplishes this!
+    @client.followers(@username).to_a.last
   end
 
   def find_user_for(username)
     # find the Twitter gem method that accomplishes this!
+    @client.user(@username)
   end
 
   def find_followers_for(user)
     # find the Twitter gem method that accomplishes this, and limit it to 10 followers only!
+    @client.followers(@username).each_with_index.select { |e, i| e if i <= 10 }
   end
 end
