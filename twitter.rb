d832306cc8a929de013de1465f29622e1f5054d4
Rigz777 <rigzgoodmusic@gmail.com>
2015-09-27 13:49:50 -0400

    still working on it

--- twitter.rb
+++ twitter.rb
@@ -17,6 +17,7 @@ class TwitterApi
 
   def most_recent_friend
     #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+    client.friends.first
   end
 
   def find_user_for(username)
